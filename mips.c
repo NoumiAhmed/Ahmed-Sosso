ebc8078932f9e0969b3f5299d3bc2ff35f10ddf6
NoumiAhmed <pro.noumi.ahmed@gmail.com>
2017-02-26 17:09:53 -0500

    lire le fichier d'entree test avec printf

---
+++ mips.c
@@ -0,0 +1,89 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define TAILLE_BUFFER 80
+
+////////declaration fonctions////////////////////////
+
+//ouvrir fichier d'entre retourne un descipteur
+FILE* ouvrirF(char* FichierEntree);
+//ouvrir fichier sortie retourne un descripteur
+FILE* ouvrirSortie(char* fichierSortie);
+//fermer un fichier
+int fermerF(FILE* fich);
+//recuperer les lignes du fichier
+
+//traduire
+int encoderVersBinaire(FILE* FichierEntree);
+
+/////////////////////////////////////////////////////
+/////////////implementation fonctions///////////////
+FILE* ouvrirF(char* FichierEntree)
+{
+  return fopen(FichierEntree, "r");
+}
+
+FILE* ouvrirSortie(char* fichierSortie)
+{
+  return fopen(fichierSortie, "wb");
+}
+
+int fermerF(FILE* fich)
+{
+  return fclose(fich);
+}
+
+ int encoderVersBinaire(FILE* fichier)
+ {
+  FILE *f = fichier;
+  char ligne[TAILLE_BUFFER + 1];
+  while (fgets(ligne, TAILLE_BUFFER, f))
+   {
+    printf("%s",ligne);
+   }
+   return 0;
+ }
+////////////////////////////////////////////////////
+int main (int argc, char** argv)
+{
+
+FILE* fichierEntree;
+FILE* fichierSortie;
+
+if (argc != 2)
+{
+  printf("erreur sur le nombre d'arguments %d \n", errno);
+  return 1;
+}
+fichierEntree = ouvrirF(argv[1]);
+
+fichierSortie = ouvrirSortie(strcat(argv[1],".asm"));
+
+if (fichierEntree == NULL)
+{
+  printf("Erreur d'ouverture de fichier d'entree %d\n", errno);
+  return 1;
+}
+
+ if (fichierSortie == NULL)
+  {
+  printf("Erreur d'ouverture de fichier de sortie%d\n", errno);
+  return 1;
+  }
+int i;
+i = encoderVersBinaire(fichierEntree);
+
+ if (fermerF(fichierEntree) != 0)
+  {
+   printf("Erreur lors de la fermeture du fichier Entree.\n");
+   return 1;
+  }
+ if (fermerF(fichierSortie) != 0)
+  {
+   printf("Erreur lors de la fermeture du fichier Sortie.\n");
+   return 1;
+  }
+
+return 0;
+}
